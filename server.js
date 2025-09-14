bac6727342e2407cf3bd953c2ce14afad723f751
DanielBritoSJunior <danielbritodasilvajunior@gmail.com>
2025-09-14 00:27:41 -0300

    criando api para cadastro de usuarios

---
+++ server.js
@@ -0,0 +1,64 @@
+import express from 'express'; // res = response, req = request
+import pkg from '@prisma/client';
+const { PrismaClient } = pkg;
+
+const prisma = new PrismaClient();
+
+const app = express();
+app.use(express.json())
+
+app.post('/usuarios', async (req, res) => { // rota para criar usuarios
+    
+    await prisma.user.create({
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+                }
+    })
+    
+    res.status(201).json(req.body) // status 201 = criado
+})
+
+app.get('/usuarios', async (req, res) => { // rota para listar usuarios
+    
+    const users = await prisma.user.findMany()
+
+    res.status(200).json(users) // status 200 = ok
+})
+
+app.put('/usuarios/:id', async (req, res) => { // rota para modificar usuarios
+    
+    await prisma.user.update({
+        where: {
+            id: req.params.id
+        },
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+                }
+    })
+    
+    res.status(201).json(req.body) // status 201 = criado
+})
+
+app.delete('/usuarios/:id', async (req, res) => { // rota para  usuarios
+    
+    await prisma.user.delete({
+        where: {
+            id: req.params.id
+        }
+    })
+    
+    res.status(204).json(req.body) // status 201 = criado
+})
+
+
+
+
+
+app.listen(3000)
+
+// USUARIO E SENHA - BANCO DE DADOS MONGODB
+// USUARIO = daniel / SENHA = y2Au6XVlMR0T6M5L
